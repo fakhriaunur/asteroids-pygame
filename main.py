d5908df0b934eda9fdde382c4714268ac45d6bd6
Fakhri <fakhri.aunur@gmail.com>
2024-10-10 15:00:46 +0700

    add turning around feature

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+                
+        player.update(dt)
         screen.fill(color=(0,0,0))
         player.draw(screen)
         pygame.display.flip()
