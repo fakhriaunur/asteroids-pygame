87a7c43efb13e60d55e60ca967597a73766be1f6
Fakhri <fakhri.aunur@gmail.com>
2024-10-10 12:50:34 +0700

    add player

--- circleshape.py
+++ circleshape.py
@@ -8,7 +8,7 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__()
         
         self.position = pygame.Vector2(x, y)
-        self.velocity = pygane.Vector2(0, 0)
+        self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -17,12 +18,14 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         print("Screen updated")
         
